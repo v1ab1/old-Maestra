dd86ab91e555a8383f653e8d109e894adb966abe
v1ab1 <sharkandmouse@gmail.com>
2022-09-15 21:00:25 +0500

    work on callback menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,27 @@
 'use strict';
 
-var DropDown = document.getElementById('dropdown');
-var DropDownButton = document.getElementById('menu_bar');
+const DropDown = document.getElementById('dropdown'),
+    DropDownIcon1 = document.getElementById('dropdown_icon1'),
+    DropDownIcon2 = document.getElementById('dropdown_icon2');
 
 function openDropDown() {
-    console.log('dasdakdsa');
     if (DropDown.style.display == "block") {
         DropDown.style.display = "none";
+        DropDownIcon2.style.display = "none";
+        DropDownIcon1.style.display = "block";
     } else {
         DropDown.style.display = "block";
+        DropDownIcon1.style.display = "none";
+        DropDownIcon2.style.display = "block";
     }
 }
-// window.onclick = function (event) {
-//     if (!event.target.matches('DropDownButton')) {
-//         document.getElementById('dropdown')
-//             .style.display = "none";
-//     }
-// };
+
+const callbackButton = document.getElementById('callback_wrapper');
+
+function openCallback() {
+    if (callbackButton.style.display == "none") {
+        callbackButton.style.display = "block";
+    } else {
+        callbackButton.style.display = "none";
+    }
+}
