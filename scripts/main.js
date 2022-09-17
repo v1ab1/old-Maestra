aa4f0a83c94ff991c0a069257c977e38c77471df
v1ab1 <sharkandmouse@gmail.com>
2022-09-17 13:36:22 +0500

    work on slider

--- scripts/main.js
+++ scripts/main.js
@@ -51,3 +51,33 @@ function closeCallback() {
         callbackDone.style.display = "none";
     }
 }
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    console.log(n);
+    slideIndex += n;
+    console.log(slideIndex);
+    showSlides(slideIndex);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    // for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace("active", "");
+    // }
+    slides[slideIndex-1].style.display = 'block';
+}
